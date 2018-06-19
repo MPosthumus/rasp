4bbad068fd3d9e81c0fe0777f3b00442c6f95ec7
Mathi <MathieuPosthumus@hotmail.com>
2018-06-19 02:33:48 +0200

    Adding   - index.php   - css      - style.css   - js     - jquery.js     - script.js

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+    <!-- Meta Keys -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CCS Files -->
+    <link type="text/css" rel="stylesheet" href="css/style.css">
+
+    <!-- Rest of head -->
+    <title>Welkom - $user</title>
+</head>
+<body>
+
+
+<!-- JavaScript Files -->
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+</body>
+</html>
